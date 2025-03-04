d3cda179d1213feba7ea1014d9bf43f153cb95b4
Noah Lackenberger <noahtobias.lackenberger@gmail.com>
2025-03-04 11:21:51 +0100

    ue01 module and service stuff

--- ue01/timer-jpms/modular-timer-beans/src/module-info.java
+++ ue01/timer-jpms/modular-timer-beans/src/module-info.java
@@ -1,3 +1,6 @@
 module modular.timer.beans {
     exports swt6.modular.beans;
+
+    provides swt6.modular.beans.TimerProvider
+            with swt6.modular.beans.impl.SimpleTimerProvider;
 }
--- ue01/timer-jpms/modular-timer-client/src/module-info.java
+++ ue01/timer-jpms/modular-timer-client/src/module-info.java
@@ -1,3 +1,5 @@
 module modular.timer.client {
     requires modular.timer.beans;
+
+    uses swt6.modular.beans.TimerProvider;
 }
